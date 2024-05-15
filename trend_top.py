3ced0e45f5e5acc1ec76340d79b9b44dd478ca14
Wellington Francisco Benicio <wellington0407@gmail.com>
2024-05-15 01:46:48 -0300

    teste

--- trend_top.py
+++ trend_top.py
@@ -2,38 +2,27 @@ import json
 import time
 import asyncio
 from fastapi import FastAPI, HTTPException
+from pytrends.request import TrendReq
 import plotly.graph_objs as go
 from fastapi.responses import HTMLResponse
-from pytrends.request import TrendReq as UTrendReq
 
 app = FastAPI()
+pytrends = TrendReq(hl='BR', tz=120)
 
-GET_METHOD = 'get'
-
-# Subclasse TrendReq com cabeçalho personalizado
-class TrendReq(UTrendReq):
-    def _get_data(self, url, method=GET_METHOD, trim_chars=0, **kwargs):
-        # Adicionando cabeçalho personalizado
-        return super()._get_data(url, method=GET_METHOD, trim_chars=trim_chars, headers=headers, **kwargs)
-
-# Cabeçalho personalizado
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
-
-async def get_google_trends(keyword, timeframe='today 5-y', geo='BR'):
+async def get_google_trends(keywords, timeframe='today 5-y', geo='BR'):
     while True:
         try:
-            pytrends = TrendReq(hl='BR', tz=120)
-            pytrends.build_payload(kw_list=[keyword], timeframe=timeframe, geo=geo)
-            interest_over_time_df = pytrends.interest_over_time()
-            return interest_over_time_df
+            trends_data = {}
+            for keyword in keywords[:5]:  # Limitando a 5 primeiras palavras-chave
+                pytrends.build_payload(kw_list=[keyword], timeframe=timeframe, geo=geo)
+                interest_over_time_df = pytrends.interest_over_time()
+                trends_data.update(extract_trend_data(keyword, interest_over_time_df))
+            return trends_data
         except Exception as e:
-            print(f"Erro ao consultar Google Trends para {keyword}: {e}")
-            await asyncio.sleep(5)  
+            print(f"Erro ao consultar Google Trends: {e}")
+            await asyncio.sleep(10)  # Esperar 10 segundos antes de tentar novamente
 
 async def get_top_trending_topics(geo='BR'):
-    pytrends = TrendReq(hl='BR', tz=120)
     trending_topics = pytrends.trending_searches(pn='brazil')
     return trending_topics
 
@@ -62,14 +51,24 @@ def plot_trend_data(trend_data):
 async def read_trending_topics():
     geo = 'BR'
     top_trends = await get_top_trending_topics()
-    lista = top_trends.values.tolist()
-    trend_data_json = {}
-    tasks = [get_google_trends(i[0], geo=geo) for i in lista]
-    trends_data = await asyncio.gather(*tasks)
-    for i, keyword in enumerate([i[0] for i in lista]):
-        trend_data_json.update(extract_trend_data(keyword, trends_data[i]))
-    plot_html = plot_trend_data(trend_data_json)
-    return plot_html
+    keywords = [i[0] for i in top_trends.values.tolist()]
+    trend_data_json = await get_google_trends(keywords, geo=geo)
+    
+    # Construir uma lista HTML das palavras-chave
+    keywords_html = "<h2>Palavras-chave tendência:</h2>"
+    keywords_html += "<ul>"
+    for keyword in keywords:
+        keywords_html += f"<li>{keyword}</li>"
+    keywords_html += "</ul>"
+    
+    # Plotar o gráfico
+    plot_html = plot_trend_data({keyword: trend_data_json[keyword] for keyword in keywords[:5]})
+    
+    # Combinar a lista de palavras-chave e o gráfico no HTML final
+    final_html = keywords_html + plot_html
+    
+    return final_html
+
 
 if __name__ == "__main__":
     import uvicorn
