fde6add47b8659a469ecf827618162c37728efc2
Wellington Francisco Benicio <wellington0407@gmail.com>
2024-05-16 00:22:59 -0300

    Revert "tratamento de erro"
    This reverts commit 8727d445c18cdcd81bf712df7d3a66b54a1e9e75.

--- trend_top.py
+++ trend_top.py
@@ -1,30 +1,15 @@
 import random
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException
 from pytrends.request import TrendReq
 import plotly.graph_objs as go
 from fastapi.responses import HTMLResponse
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 app = FastAPI()
 pytrends = TrendReq(hl='BR', tz=120)
 
-nltk.download('vader_lexicon')
-nltk.download('stopwords')
-nltk.download('punkt')
-
-sia = SentimentIntensityAnalyzer()
-stop_words = set(stopwords.words('portuguese'))
-
 async def get_top_trending_topics(geo='BR'):
-    try:
-        trending_topics = pytrends.trending_searches(pn='brazil')
-        return trending_topics
-    except Exception as e:
-        print(f"Erro ao obter tópicos em alta: {e}")
-        return None
+    trending_topics = pytrends.trending_searches(pn='brazil')
+    return trending_topics
 
 async def get_google_trends(keyword, timeframe='today 5-y', geo='BR', max_retries=3):
     for _ in range(max_retries):
@@ -57,26 +42,14 @@ def plot_trend_data(trend_data):
         plots_html.append(fig.to_html(full_html=False, include_plotlyjs='cdn'))
     return ''.join(plots_html)
 
-def analyze_trend(keyword):
-    try:
-        news_articles = pytrends.related_queries(kw_list=[keyword])['top']
-        analysis = ""
-        for i in range(5):
-            sentence = news_articles.iloc[i]['value']
-            words = word_tokenize(sentence)
-            filtered_words = [w for w in words if w not in stop_words]
-            analysis += ' '.join(filtered_words)
-            sentiment = sia.polarity_scores(sentence)
-            analysis += f" - Sentiment: {sentiment['compound']}"
-        return analysis
-    except Exception as e:
-        return f"Análise de tendência falhou: {e}"
-
 @app.get("/trending-topics/", response_class=HTMLResponse)
-async def read_trending_topics(keyword: str = Query(None)):
+async def read_trending_topics():
     geo = 'BR'
     top_trends = await get_top_trending_topics()
-    
+    keywords = [i[0] for i in top_trends.values.tolist()]
+    random_keyword = random.choice(keywords)
+    trend_data = await get_google_trends(random_keyword, geo=geo)
+
     with open("style/styles.css", "r") as css_file:
         css_content = css_file.read()
 
@@ -85,33 +58,20 @@ async def read_trending_topics(keyword: str = Query(None)):
         <title>Trending Topics</title>
         <style>{css_content}</style>
     </head>
+    <h2>Trending Topics:</h2>
+    <ul class="keyword-list">
     """
 
-    if not top_trends.empty:
-        keywords = [i[0] for i in top_trends.values.tolist()]
-        keywords_html += f"<h2>Trending Topics:</h2><ul class='keyword-list'>"
-        for keyword in keywords:
-            keywords_html += f'<li class="keyword-list-item">{keyword}</li>'
-        keywords_html += "</ul>"
-    else:
-        keywords_html += "<h2>Trending Topics:</h2><p>Não foi possível obter tópicos em alta.</p>"
+    for keyword in keywords:
+        keywords_html += f'<li class="keyword-list-item">{keyword}</li>'
 
-    if keyword is None:
-        if not top_trends.empty:
-            random_keyword = random.choice(keywords)
-            trend_data = await get_google_trends(random_keyword, geo=geo)
-        else:
-            trend_data = None
-    else:
-        trend_data = await get_google_trends(keyword, geo=geo)
-
+    keywords_html += "</ul>"
+    
     if trend_data:
-        plot_html = plot_trend_data({keyword: trend_data})
-        analysis = analyze_trend(keyword)
-        final_html = f"{keywords_html}<br>{plot_html}<br><br><b>Análise:</b><br>{analysis}"
+        plot_html = plot_trend_data({random_keyword: trend_data})
+        final_html = f"{keywords_html}<br>{plot_html}"
     else:
-        analysis = analyze_trend(keyword)
-        final_html = f"{keywords_html}<br><br><b>Análise:</b><br>{analysis}"
+        final_html = keywords_html
 
     return final_html
 
