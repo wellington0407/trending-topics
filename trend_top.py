7d30a4d13e058cfa1fafe04fab6d3658dc707f84
Wellington Francisco Benicio <wellington0407@gmail.com>
2024-05-15 23:16:03 -0300

    tratamento de erro

--- trend_top.py
+++ trend_top.py
@@ -1,7 +1,6 @@
 import random
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pytrends.request import TrendReq
-import plotly.graph_objs as go
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -21,7 +20,6 @@ async def get_google_trends(keyword, timeframe='today 5-y', geo='BR', max_retrie
                 return trend_data
         except Exception as e:
             print(f"Erro ao consultar Google Trends: {e}")
-    print(f"Dado de tendência indefinido: {keyword}")
     return None
 
 @app.get("/trending-topics/", response_class=HTMLResponse)
@@ -32,30 +30,29 @@ async def read_trending_topics():
     random_keyword = random.choice(keywords)
     trend_data = await get_google_trends(random_keyword, geo=geo)
 
+    with open("style/styles.css", "r") as css_file:
+        css_content = css_file.read()
+
+    keywords_html = f"""
+    <head>
+        <title>Trending Topics</title>
+        <style>{css_content}</style>
+    </head>
+    <h2>Trending Topics:</h2>
+    <ul class="keyword-list">
+    """
+
+    for keyword in keywords:
+        keywords_html += f'<li class="keyword-list-item">{keyword}</li>'
+
+    keywords_html += "</ul>"
+    
     if trend_data:
         plot_html = plot_trend_data({random_keyword: trend_data})
-        
-        with open("style/styles.css", "r") as css_file:
-            css_content = css_file.read()
-        
-        keywords_html = f"""
-        <head>
-            <title>Trending Topics</title>
-            <style>{css_content}</style>
-        </head>
-        <h2>Trending Topics:</h2>
-        <ul class="keyword-list">
-        """
-        
-        for keyword in keywords:
-            keywords_html += f'<li class="keyword-list-item">{keyword}</li>'
-        
-        keywords_html += "</ul>"
-        
         final_html = f"{keywords_html}<br>{plot_html}"
     else:
-        final_html = "<h2>Dado de tendência indefinido</h2>"
-    
+        final_html = keywords_html
+
     return final_html
 
 def extract_trend_data(keyword, trends_data):
@@ -64,20 +61,7 @@ def extract_trend_data(keyword, trends_data):
         dates = [str(date) for date in daily_counts.index]
         counts = daily_counts.values.tolist()
         return {'dates': dates, 'counts': counts}
-    else:
-        print(f"Dado de tendência indefinido: {keyword}")
-        return {}
-
-def plot_trend_data(trend_data):
-    plots_html = []
-    for keyword, data_info in trend_data.items():
-        dates = data_info['dates']
-        counts = data_info['counts']
-        data = [go.Scatter(x=dates, y=counts, mode='lines+markers', name=keyword, line=dict(width=2), marker=dict(size=8))]
-        layout = go.Layout(title=f'Google Trends: {keyword}', xaxis=dict(title='Date', tickfont=dict(size=14)), yaxis=dict(title='Count', tickfont=dict(size=14)), titlefont=dict(size=16))
-        fig = go.Figure(data=data, layout=layout)
-        plots_html.append(fig.to_html(full_html=False, include_plotlyjs='cdn'))
-    return ''.join(plots_html)
+    return {}
 
 if __name__ == "__main__":
     import uvicorn
